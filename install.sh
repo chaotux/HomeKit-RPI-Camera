3a43b27e824b09e23788d829222458b7186f8264
chaotux <chaotux@gmail.com>
2016-11-28 21:31:17 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo "You must enable camera before running this script"
-sleep 5
+echo "start_x=1" >> /boot/config.txt
+echo "gpu_mem=128" >> /boot/config.txt
 #Install required packages
 sudo apt-get install git build-essential libavahi-compat-libdnssd-dev
+
+#Install RPi-Cam-Web-Interface (from http://elinux.org/RPi-Cam-Web-Interface)
+cd ~ ; git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git && cd RPi_Cam_Web_Interface && chmod u+x *.sh
+/home/pi/RPi_Cam_Web_Interface/install.sh
 
 #Install updated node
 cd ~; wget http://node-arm.herokuapp.com/node_latest_armhf.deb && sudo dpkg -i node_latest_armhf.deb && rm node_latest_armhf.deb && sudo npm install -g node-gyp;
@@ -17,11 +21,7 @@ cd ~/HAP-NodeJS/lib/ && sudo mv Camera.js OriginalCamera.js.bak && wget "https:/
 sudo npm install node-persist debug mdns fast-srp-hap ed25519 buffer-shims curve25519-n ip node-cmd
 
 #Install GPU accelerated ffmpeg (might not be needed for the setup in the end)
-cd ~/ ; wget https://github.com/ccrisan/motioneye/wiki/precompiled/ffmpeg_3.1.1-1_armhf.deb && sudo dpkg -i ffmpeg_3.1.1-1_armhf.deb && rm ffmpeg_3.1.1-1_armhf.deb
+cd ~; wget https://github.com/ccrisan/motioneye/wiki/precompiled/ffmpeg_3.1.1-1_armhf.deb && sudo dpkg -i ffmpeg_3.1.1-1_armhf.deb && rm ffmpeg_3.1.1-1_armhf.deb
 
 #Now create custom task script (placed /home/pi/HAP-NodeJS/task) to use RPi-Cam-Web-Interface to update the snapshot
-cd /home/pi/HAP-NodeJS/ && wget https://raw.githubusercontent.com/chaotux/HomeKit-RPI-Camera/master/task && sudo chmod a+x /home/pi/HAP-NodeJS/task
-
-#Install RPi-Cam-Web-Interface (from http://elinux.org/RPi-Cam-Web-Interface)
-cd ~ ; git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git && cd RPi_Cam_Web_Interface && chmod u+x *.sh
-/home/pi/RPi_Cam_Web_Interface/install.sh
+cd ~/HAP-NodeJS/ && wget https://raw.githubusercontent.com/chaotux/HomeKit-RPI-Camera/master/task && sudo chmod a+x /home/pi/HAP-NodeJS/task
